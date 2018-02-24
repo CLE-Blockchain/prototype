6c00e080d0e1b47d6fef133b20ff8e3f19b92a7e
Luke Espina <luke.espina@gmail.com>
2018-02-24 10:56:59 -0500

    Add Provider and Router

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Hello World!</h1>, root);
+  ReactDOM.render(<App />, root);
 });
