2c5bac83e9742791eaa89c9d9802c4cbb3fb7ad3
Luke Espina <luke.espina@gmail.com>
2018-02-24 10:50:52 -0500

    Add static root page with js entry

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ plugins = plugins.concat(
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/bass_case.jsx",
+  entry: "./frontend/index.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
