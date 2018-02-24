e5b4f036027e907e661635ce356b8a6aacc9a94f
Luke Espina <luke.espina@gmail.com>
2018-02-24 11:17:26 -0500

    Restructure frontend file structure

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./components/app";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
