939671b2454681b67d6f97b017b369097921003f
Sunny Wong <swong194@gmail.com>
2018-02-24 19:52:10 -0500

    adds landing page css

--- frontend/components/landing/Landing.jsx
+++ frontend/components/landing/Landing.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default class Landing extends React.Component {
   render() {
     return (
       <section className="landing">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <div className="container">
+            <NavLink className="navbar-brand" to="/">
+              Credible
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+          </div>
+        </nav>
         <Link className="landing__lawyer" to="/lawyer">
           <p>Lawyer</p>
         </Link>
